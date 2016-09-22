f3257790290934dccd6f343ead832e63b178cfa7
J. Ryan Conklin <jryanconklin@gmail.com>
2016-09-22 14:44:00 -0700

    Add Save, Delete and GetAll for Cuisine Class.

--- src/Cuisine.php
+++ src/Cuisine.php
@@ -3,47 +3,58 @@
     class Cuisine
     {
 //Properties
-    private $id;
-    private $style;
+        private $id;
+        private $style;
 
 //Constructor
-    function __construct($id = null, $style)
-    {
-        $this->id = $id;
-        $this->style = $style;
-    }
+        function __construct($id = null, $style)
+        {
+            $this->id = $id;
+            $this->style = $style;
+        }
 
 //Getter and Setter Methods
-    function getId()
-    {
-        return $this->id;
-    }
+        function getId()
+        {
+            return $this->id;
+        }
 
-    function getStyle()
-    {
-        return $this->style;
-    }
+        function getStyle()
+        {
+            return $this->style;
+        }
 
-    function setStyle($new_style)
-    {
-        $this->style = (string) $new_style;
-    }
+        function setStyle($new_style)
+        {
+            $this->style = (string) $new_style;
+        }
 
 //Regular Methods
-
-
-
-
-
+        function save()
+        {
+            $GLOBALS['DB']->exec("INSERT INTO cuisines (style) VALUES ('{$this->getStyle()}')");
+            $this->id = $GLOBALS['DB']->lastInsertId();
+        }
 
 
 //Static Methods
+        static function getAll()
+        {
+            $returned_cuisines = $GLOBALS['DB']->query("SELECT * FROM cuisines;");
+            $cuisines = array();
+            foreach($returned_cuisines as $cuisine) {
+                $id = $cuisine['id'];
+                $style = $cuisine['style'];
+                $new_cuisine = new Cuisine($id, $style);
+                array_push($cuisines, $new_cuisine);
+            }
+            return $cuisines;
+        }
 
-
-
-
-
-
+        static function deleteAll()
+        {
+            $GLOBALS['DB']->exec("DELETE FROM cuisines;");
+        }
 
     }
 ?>
--- tests/CuisineTest.php
+++ tests/CuisineTest.php
@@ -11,6 +11,12 @@
     class CuisineTest extends PHPUnit_Framework_TestCase
     {
 
+        protected function tearDown()
+        {
+            Cuisine::deleteAll();
+        }
+
+
         function test_getId() //Read Cuisine Style
         {
             //Arrange
@@ -53,6 +59,60 @@
 
             //Assert
             $this->assertEquals($new_style, $result);
+        }
+
+        function test_save()
+        {
+            //Arrange
+            $id = null;
+            $style = "Thai";
+            $test_cuisine = new Cuisine($id, $style);
+            $test_cuisine->save();
+
+            //Act
+            $result = Cuisine::getAll();
+
+            //Assert
+            $this->assertEquals($test_cuisine, $result[0]);
+        }
+
+        function test_getAll()
+        {
+            //Arrange
+            $id1 = null;
+            $id2 = null;
+            $style1 = "Thai";
+            $style2 = "Northern Thai";
+            $test_cuisine1 = new Cuisine($id1, $style1);
+            $test_cuisine1->save();
+            $test_cuisine2 = new Cuisine($id2, $style2);
+            $test_cuisine2->save();
+
+            //Act
+            $result = Cuisine::getAll();
+            //Assert
+            $this->assertEquals([$test_cuisine1, $test_cuisine2], $result);
+
+        }
+
+        function test_deleteAll()
+        {
+            //Arrange
+            $id1 = null;
+            $id2 = null;
+            $style1 = "Thai";
+            $style2 = "Northern Thai";
+            $test_cuisine1 = new Cuisine($id1, $style1);
+            $test_cuisine1->save();
+            $test_cuisine2 = new Cuisine($id2, $style2);
+            $test_cuisine2->save();
+
+            //Act
+            Cuisine::deleteAll();
+            $result = Cuisine::getAll();
+
+            //Assert
+            $this->assertEquals([], $result);
         }
 
 
